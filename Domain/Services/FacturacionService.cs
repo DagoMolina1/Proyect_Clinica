f81609bffb31880c8054dbf35f1b4c998398da33
DagoMolina09 <99810953+DagoMolina1@users.noreply.github.com>
2025-09-22 21:31:26 -0500

    Move application services into domain layer

--- Domain/Services/FacturacionService.cs
+++ Domain/Services/FacturacionService.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using ClinicaIPS_U.Domain.Entities;
 using ClinicaIPS_U.Domain.Interfaces;
 
-namespace ClinicaIPS_U.Application.Services {
+namespace ClinicaIPS_U.Domain.Services {
     public class FacturacionService {
         private readonly IFacturacionRepository _facturacionRepository;
 
